23ec2de6b738d7fb3f156adc1370a470194ffdcd
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-06 20:13:41 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,13 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.implementation.SellerDaoJDBC;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
   public static void main(String[] args) {
-    Department department = new Department(1, "books");
-    Seller seller = new Seller(12, "joao", "joao@gmail.com", new Date(), 3000.0, department);
     SellerDao sellerDao = DaoFactory.createSellerDao();
+
+    Seller seller = sellerDao.findById(1);
 
     System.out.println(seller);
   }
