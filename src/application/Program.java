7f4bd4a9a55b5678135a6edb4d17918521661ff3
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-06 19:15:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
   public static void main(String[] args) {
     Department department = new Department(1, "books");
-    System.out.println(department);
+    Seller seller = new Seller(12, "joao", "joao@gmail.com", new Date(), 3000.0, department);
+
+    System.out.println(seller);
   }
 }
